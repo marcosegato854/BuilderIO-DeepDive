46ee2e351ec30849297a3bea9333ed6a4fd43ac8
SEGATO Marco <marcosegato854@gmail.com>
2025-07-17 09:00:39 +0200

    Removed any reference from App.js

--- main-project/src/App.jsx
+++ main-project/src/App.jsx
@@ -1,44 +1,10 @@
 import { useState } from 'react';
-import {
-  CustomButton,
-  UserInfo,
-  ImageBox,
-  PreferencesList,
-} from './components';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <UserInfo birthDate='08/05/2004' name='Marco Segato' />
-
-      <CustomButton
-        label='Modifica'
-        size='big'
-        variant='outlined'
-        color='#fff'
-      />
-      <CustomButton
-        label='Save'
-        size='small'
-        variant='contained'
-        color='primary'
-      />
-      <CustomButton
-        label='Delete'
-        size='default'
-        variant='contained'
-        color='error'
-      />
-      <ImageBox src='' />
-      <PreferencesList
-        label='Preferenze'
-        preferences={['preferenza 1', 'preferenza 2', 'preferenza 3']}
-      />
-    </>
-  );
+  return <>Hello World!</>;
 }
 
 export default App;
